f2f76a0d1a26db25d148570ce3a3a4c2877f79d7
Marimuthu Raju <risingknights@Marimuthus-MBP.fios-router.home>
2022-05-05 09:59:53 -0400

    Uplaod latest

--- rds.tf
+++ rds.tf
@@ -1,12 +1,4 @@
-resource "aws_db_instance" "default" {
-  allocated_storage    = 5
-  storage_type         = "gp2"
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  db_name              = "mydb"
-  username             = "foo"
-  password             = "${file("../rds_pass.txt")}"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot = "true"
+resource "aws_instance" "myec2" {
+   ami = "ami-0f9fc25dd2506cf6d"
+   instance_type = "t2.micro"
 }
